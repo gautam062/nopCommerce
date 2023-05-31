bc9ab1255a790671a2350f0438c751e8e8d82a3b
Gautam Kumar <gautam.kumar062@gmail.com>
2023-05-31 14:31:47 +0200

    updated run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 pytest -v -m "sanity" --html=./Reports/reports.html .\testCases\ --browser chrome
-REM pytest -v -m "regression" --html=./Reports/reports.html .\testCases\ --browser chrome
+pytest -v -m "regression" --html=./Reports/reports.html .\testCases\ --browser firefox
 REM pytest -v -m "sanity and regression" --html=./Reports/reports.html .\testCases\ --browser chrome
 REM pytest -v -m "sanity or regression" --html=./Reports/reports.html .\testCases\ --browser chrome
