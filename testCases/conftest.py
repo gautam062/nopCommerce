abefbac673d86411a0a41efbeeb48f2194de4b9a
Gautam Kumar <gautam.kumar062@gmail.com>
2023-05-31 15:43:27 +0200

    code fix

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,5 +1,7 @@
+import os
 from selenium import webdriver
 import pytest
+import pytest_html
 
 
 @pytest.fixture()
@@ -41,3 +43,26 @@ def browser(request):
 #     metadata.pop("JAVA_HOME", None)
 #     metadata.pop("Plugins", None)
 
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     report = outcome.get_result()
+#     extras = getattr(report, "extras", [])
+#     if report.when == "call":
+#         # always add url to report
+#         extras.append(pytest_html.extras.url("https://admin-demo.nopcommerce.com/"))
+#         xfail = hasattr(report, "wasxfail")
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             # only add additional html on failure+
+#             report_directory = os.path.dirname(item.config.option.htmlpath)
+#             file_name = report.nodeid.replace("::", "_") + ".png"
+#             destFile = os.path.join(report_directory, file_name)
+#             driver.save_screenshot(destFile)
+#             if file_name:
+#                 html= '<div><img src=%s" alt="screenshot" style = "width:300ox;height=200px"'\
+#                 'onlick="window.open(this.src" align="right"/></div>'%file_name
+#             extras.append(pytest_html.extras.html(html))
+#         report.extras = extras
+
+def pytest_html_report_title(report):
+    report.title = "Nop Commerce Automation Report"
